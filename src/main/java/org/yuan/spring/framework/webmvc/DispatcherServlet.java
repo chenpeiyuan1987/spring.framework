7668b96718d6dcb6def7a7433ee714a45f6d2bc9
chenpeiyuan1987 <chenpeiyuan1987@163.com>
2019-10-29 02:34:07 +0800

    Edit DispatcherServlet

--- src/main/java/org/yuan/spring/framework/webmvc/DispatcherServlet.java
+++ src/main/java/org/yuan/spring/framework/webmvc/DispatcherServlet.java
@@ -1,5 +1,8 @@
 package org.yuan.spring.framework.webmvc;
 
+import lombok.extern.slf4j.Slf4j;
+import org.yuan.spring.framework.annotation.Controller;
+import org.yuan.spring.framework.annotation.RequestMapping;
 import org.yuan.spring.framework.context.ApplicationContext;
 
 import javax.servlet.ServletConfig;
@@ -7,12 +10,17 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+@Slf4j
 public class DispatcherServlet extends HttpServlet {
 
     private final String LOCATION = "contextConfigLocation";
@@ -43,9 +51,156 @@ public class DispatcherServlet extends HttpServlet {
 
     @Override
     public void init(ServletConfig config) throws ServletException {
-        super.init(config);
+        context = new ApplicationContext(config.getInitParameter(LOCATION));
+        initStrategies(context);
     }
 
-    private void doDispatch(HttpServletRequest req, HttpServletResponse resp) {
+    private void initStrategies(ApplicationContext context) {
+        initMultipartResolver(context);
+        initLocaleResolver(context);
+        initThemeResolver(context);
+        initHandlerMappings(context);
+        initHandlerAdapters(context);
+        initHandlerExceptionResolvers(context);
+        initRequestToViewNameTranslator(context);
+        initViewResolvers(context);
+        initFlashMapManager(context);
+    }
+
+    private void initFlashMapManager(ApplicationContext context) {
+    }
+
+    private void initViewResolvers(ApplicationContext context) {
+        String templateRoot = context.getConfig().getProperty("templateRoot");
+        String templateRootPath = this.getClass().getClassLoader().getResource(templateRoot).getFile();
+        File templateRootDir = new File(templateRootPath);
+        for (File template : templateRootDir.listFiles()) {
+            this.viewResolvers.add(new ViewResolver(templateRoot));
+        }
+    }
+
+    private void initRequestToViewNameTranslator(ApplicationContext context) {
+    }
+
+    private void initHandlerExceptionResolvers(ApplicationContext context) {
+    }
+
+    private void initHandlerAdapters(ApplicationContext context) {
+        for (HandlerMapping handlerMapping : this.handlerMappings) {
+            this.handlerAdapters.put(handlerMapping, new HandlerAdapter());
+        }
+    }
+
+    private void initHandlerMappings(ApplicationContext context) {
+        String[] beanNames = context.getBeanDefinitionNames();
+        try {
+            for (String beanName : beanNames) {
+                Object controller = context.getBean(beanName);
+                Class<?> clazz = controller.getClass();
+                if (!clazz.isAnnotationPresent(Controller.class)) {
+                    continue;
+                }
+
+                String baseUrl = "";
+                if (clazz.isAnnotationPresent(RequestMapping.class)) {
+                    RequestMapping requestMapping = clazz.getAnnotation(RequestMapping.class);
+                    baseUrl = requestMapping.value();
+                }
+
+                Method[] methods = clazz.getMethods();
+                for (Method method : methods) {
+                    if (!method.isAnnotationPresent(RequestMapping.class)) {
+                        continue;
+                    }
+
+                    RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+                    String regex = ("/" + baseUrl + requestMapping.value()
+                            .replaceAll("\\*", ".*")
+                            .replaceAll("/+", "/"));
+                    Pattern pattern = Pattern.compile(regex);
+                    this.handlerMappings.add(new HandlerMapping(controller, pattern, method));
+                    log.info("Mapping: " + regex + ", " + method);
+                }
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void initThemeResolver(ApplicationContext context) {
+    }
+
+    private void initLocaleResolver(ApplicationContext context) {
+    }
+
+    private void initMultipartResolver(ApplicationContext context) {
+    }
+
+    private void doDispatch(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        HandlerMapping handler = getHandler(req);
+        if (handler == null) {
+            processDispatchResult(req, resp, new ModelAndView("404"));
+            return;
+        }
+
+        HandlerAdapter ha = getHandlerAdapter(handler);
+
+        ModelAndView mv = ha.handle(req, resp, handler);
+
+        processDispatchResult(req, resp, mv);
+    }
+
+    private HandlerAdapter getHandlerAdapter(HandlerMapping handler) {
+        if (this.handlerAdapters.isEmpty()) {
+            return null;
+        }
+
+        HandlerAdapter ha = this.handlerAdapters.get(handler);
+        if (ha.supports(handler)) {
+            return ha;
+        }
+
+        return null;
+    }
+
+    private void processDispatchResult(HttpServletRequest req, HttpServletResponse resp, ModelAndView mv) throws Exception {
+        if (mv == null) {
+            return;
+        }
+
+        if (this.viewResolvers.isEmpty()) {
+            return;
+        }
+
+        if (this.viewResolvers != null) {
+            for (ViewResolver viewResolver : this.viewResolvers) {
+                View view = viewResolver.resolveViewName(mv.getViewName(), null);
+                if (view != null) {
+                    view.render(mv.getModel(), req, resp);
+                    return;
+                }
+            }
+        }
+    }
+
+    private HandlerMapping getHandler(HttpServletRequest req) {
+        if (this.handlerMappings.isEmpty()) {
+            return null;
+        }
+
+        String url = req.getRequestURI();
+        String contextPath = req.getContextPath();
+        url = url.replace(contextPath, "").replaceAll("/+", "/");
+
+        for (HandlerMapping handler : this.handlerMappings) {
+            Matcher matcher = handler.getPattern().matcher(url);
+            if (!matcher.matches()) {
+                continue;
+            }
+            return handler;
+        }
+
+        return null;
     }
 }
